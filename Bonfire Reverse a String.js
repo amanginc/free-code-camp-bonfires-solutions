30d21ce5516ffb125ae68de1b83cd36d94c016fb
Andrew Chan <amanginc@gmail.com>
2015-09-10 14:53:47 -0400

    Update Bonfire Reverse a String.js

--- Bonfire Reverse a String.js
+++ Bonfire Reverse a String.js
@@ -1,12 +1,34 @@
 function reverseString(str) {
-  //FUNCTIONAL Programming
-  //return str.split('').reverse().join(''); 
-  //HARDCODE BY USING FOR LOOP TO REVERSE
+  /*FUNCTIONAL Programming using functions
+  
+  return str.split('').reverse().join('');
+  
+  For Loop solution 
+  
   var result = '';
   for (var i = str.length-1; i >= 0; i--){
     result += str[i];
   }
   return result;
+  */
+  /*While Loop solution
+  
+  var result = '';
+  var i = str.length;
+  while (i-- > 0){
+    result+=str[i];
+  }
+  return result;
+  
+  Recursion w/o ternary operator
+  
+  if (!str) return '';
+  return reverseString(str.slice(1)) + str[0];
+  */
+  //Recursion with ternary operator
+  
+  return str ? reverseString(str.slice(1)) + str[0] : '';
+  
 }
 
 reverseString('htraE morf sgniteerG');
